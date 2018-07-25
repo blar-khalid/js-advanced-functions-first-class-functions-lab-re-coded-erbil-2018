52553ac8f2ac45da19a7b76d6b9a4409152b767a
blar khalid <blarharki@gmail.com>
2018-07-25 13:45:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const createFareMultiplier=function (val){
  let  fareTripler=function (val2){
    return val2 *3;
  }
-const selectDifferentDrivers =function(driver,returnLastTwoDrivers){
-  return returnLastTwoDrivers;
+const selectDifferentDrivers =function(driver,returnFirstTwoDrivers){
+  return returnFirstTwoDrivers;
   
 }
