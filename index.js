0478004d17c580a98638f78026fe11257c092eae
blar khalid <blarharki@gmail.com>
2018-07-25 13:03:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+ const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+const returnFirstTwoDrivers=function(driversAray){
+  return driversAray.slice(0,2);
+}
+const returnLastTwoDrivers=function(driversAray){
+  return driversAray.slice(2);
+}
+
+let selectingDrivers =[(returnFirstTwoDrivers),(returnLastTwoDrivers)];
+
+function createFareMultiplier(){}
+const fareMultiplier = createFareMultiplier(2);
